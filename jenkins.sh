2855a8d1024672d3068df6a79586c354f3c1afea
André Boddenberg <andre.boddenberg@gmx.de>
2017-04-10 14:06:19 +0200

    Add verify_value_string_arrays_are_terminated.py call again.
    It got lost somehow...

--- jenkins.sh
+++ jenkins.sh
@@ -6,6 +6,7 @@ source osmo-build.sh
 genericDeps() {
 
 	x="$($1 libosmocore master ac_cv_path_DOXYGEN=false)"
+	"$deps"/libosmocore/contrib/verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 	x="${x}_$($1 libosmo-abis)"
 	x="${x}_$($1 libosmo-netif)"
 	x="${x}_$($1 libosmo-sccp)"
